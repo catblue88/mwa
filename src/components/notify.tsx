8b7aff5119a2fb872812b3b996ecf21f3b8c2e56
catblue88 <pariscatblue88@googlemail.com>
2023-04-02 00:55:56 +0100

    4th initial...

--- src/components/notify.tsx
+++ src/components/notify.tsx
@@ -6,7 +6,7 @@ import { Link } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import type { VariantType } from 'notistack';
 import { useSnackbar } from 'notistack';
-import React, { useCallback } from 'react';
+import React, { useCallback } from 'react'; 
 
 const Notification = styled('span')(() => ({
     display: 'flex',
